d92487bf6f43589202382b764f0bce916f2fac77
chjason00 <110322889+chjason00@users.noreply.github.com>
2025-01-26 19:03:12 +0900

    250126

--- transaction/views.py
+++ transaction/views.py
@@ -36,3 +36,9 @@ def delete(request, pk):
         item.delete()
         return redirect('transaction:list')
     return render(request, 'transaction/delete_transaction.html', {'transaction': item})
+
+def sum(request):
+    total = 0
+    for transaction in Transaction.objects.all():
+        total += transaction.amount
+    return total;
